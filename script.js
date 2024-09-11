0dfd2173f074cfb5500149636241d0a29766d90e
Luke Lopez <lukelopez499@Skywalkers>
2024-09-11 15:47:29 -0500

    JS Code is done, finishing second design

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-document.getElementById('change-style-btn').addEventListener('click', function() {
-    var link = document.getElementById('theme-stylesheet');
-    if (link.getAttribute('href') === 'style.css') {
-        link.setAttribute('href', 'style2.css');
-    } else {
-        link.setAttribute('href', 'style.css');
-    }
-});
+function ChangeTheme(){
+    
+        var link = document.getElementById('theme-stylesheet');
+        if (link.getAttribute('href') === 'style.css') {
+            link.setAttribute('href', 'style2.css');
+            localStorage.setItem("StyleChosen", "style2.css")
+        } else {
+            localStorage.setItem("StyleChosen", "style.css")
+            link.setAttribute('href', 'style.css');
+        }
+    
+}
+function FindTheme(){
+    document.getElementById("theme-stylesheet").setAttribute("href" , localStorage.getItem("StyleChosen"))
+}
+
